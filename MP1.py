bd8ada5594a397329692e003ec730c1edbf34f34
Khushi Sidana <khushisidana@Khushis-MacBook-Air-3.local>
2024-01-22 16:19:03 -0600

    Updated Solution

--- MP1.py
+++ MP1.py
@@ -2,6 +2,7 @@ import random
 import os
 import string
 import sys
+import re
 
 stopWordsList = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours",
             "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its",
@@ -16,6 +17,23 @@ stopWordsList = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "y
 
 delimiters = " \t,;.?!-:@[](){}_*/"
 
+def get_titles():
+    titles = []
+    for line in sys.stdin:
+        titles.append(line.strip())
+    return titles
+
+def process_title(title, delimiters):
+    delimeter_pattern= "[" + re.escape(delimiters) + "]"
+    words = re.split(delimeter_pattern, title)
+    processed_words=[word.lower().strip() for word in words if word.lower().strip() not in stopWordsList]
+    return  processed_words
+
+
+    
+
+
+
 def getIndexes(seed):
     random.seed(seed)
     n = 10000
@@ -27,10 +45,26 @@ def getIndexes(seed):
 
 def process(userID):
     indexes = getIndexes(userID)
-    ret = []
-    # TODO
-                    
-    for word in ret:
-        print(word)
+    word_freq = {}
+   
+    
+    titles = get_titles()
+    for index in indexes:
+        title = titles[index]
+        processed_words = process_title(title, delimiters)
+
+        # Count word frequencies
+        for word in processed_words:
+            if word in word_freq:
+                word_freq[word] += 1
+            else:
+                word_freq[word] = 1
+
+    # Sort words by frequency in descending order and select top 20
+    top_20_words = sorted(word_freq.items(), key=lambda x: (-x[1], x[0]))[1:21]
+
+    # Print or return the top 20 words and their frequencies
+    for word, freq in top_20_words:
+        print(f"{word}")
 
 process(sys.argv[1])
